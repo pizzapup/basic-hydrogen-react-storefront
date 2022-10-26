8695ede2135e8dc36247c742bb39baa278eaed53
pizzapup_ <hardwickariella@gmail.com>
2022-10-26 18:32:51 -0400

    added tailwindcss styling

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world!</h1>
-      <p>Welcome to Hydrogen.</p>
+    <section className="p-6 md:p-8 lg:p-12">
+      <h1 className="font-extrabold mb-4 text-5xl md:text-7xl">Hello world!</h1>
+      <p className="font-bold mb-3">Welcome to Hydrogen.</p>
       <p>
         Hydrogen is a front-end web development framework used for building
         Shopify custom storefronts.
       </p>
-    </div>
+    </section>
   );
 }
